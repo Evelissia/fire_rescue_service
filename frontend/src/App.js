41da54ca292ac6787c94c94e86183262355c40c5
Evelissia <evelissia@gmail.com>
2024-03-22 20:57:58 +0600

    init react app

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { Header } from './components/header/index.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <>
+        <Header />
+      </>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/header/index.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import styles from './Header.module.css';
+import { Container } from '@mui/material';
+
+export const Header = () => {
+  return (
+    <div className={styles.root}>
+      <Container maxWidth="lg">
+        <div className={styles.inner}>
+          <a className={styles.logo} href="/">
+            <div>LOGO</div>
+          </a>
+          <div className={styles.menuButton}>
+            <button>О компании</button>
+            <button>Услуги</button>
+            <button>Отправить заявку</button>
+            <button>Статус заявки</button>
+          </div>
+          <div className={styles.buttons}>
+            <button variant="outlined" className={`${styles.btn1} ${styles.button}`}>
+              Войти
+            </button>
+            <button variant="contained" className={`${styles.btn2} ${styles.button}`}>
+              Создать аккаунт
+            </button>
+          </div>
+        </div>
+      </Container>
+    </div>
+  );
+};
